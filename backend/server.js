84ba1c2786b14439de7185f67fb0b786987de08b
Paul-Nathan <pngotte@gmail.com>
2024-10-02 14:08:47 +0100

    Connecting MongoDB to Express using .env succesfully

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/connectDB");
+const mongoose = require("mongoose");
 
 const app = express();
-
 
 //Routes
 app.get("/", (req, res) =>{
     res.send("HomePage");
 });
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            connectDB();
+            app.listen(PORT, ()=>{
+                console.log(`Server running on port ${PORT}`);
+            });
+        })
+        .catch((err) => console.log(error));
+
+
