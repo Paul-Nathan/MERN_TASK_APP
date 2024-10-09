0b2ae8b368cdc859aa81b77e87b6269eb5060d38
Paul-Nathan <pngotte@gmail.com>
2024-10-09 15:49:29 +0100

    Middleware

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,36 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/connectDB");
 const mongoose = require("mongoose");
+const Task = require("./model/taskModel");
+
 
 const app = express();
+app.use(express.json())
+
+//Middleware
+// const logger = (req, res, next) => {
+//     console.log("Middleware ran");
+//     console.log(req.method);
+//     next();
+// };
 
 //Routes
 app.get("/", (req, res) =>{
     res.send("HomePage");
 });
+
+//Create a Task
+app.post("/api/tasks", async (req, res) => {
+   try {
+    const tasks = await Task.create(req.body)
+    res.status(200).json(tasks)
+   } catch (error) {
+    res.status(500).json({error: error.message})
+   }
+})
+
+//Read a Task
+app.get("")
 
 const PORT = process.env.PORT || 5000;
 
